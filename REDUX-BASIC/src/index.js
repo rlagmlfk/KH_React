0dfa927cee034ad030abb520cbf52eca20bc7173
rlagmlfk <gmlfk703@gmail.com>
2022-10-18 14:02:55 +0900

    리덕스 셋팅

--- REDUX-BASIC/src/index.js
+++ REDUX-BASIC/src/index.js
@@ -4,10 +4,17 @@ import App from "./App";
 import TomatoTalk from "./components/talk/TomatoTalk";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "@fortawesome/fontawesome-free/js/all.js";
+import { legacy_createStore } from "redux";
+import { Provider } from "react-redux";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+// store에 담긴 상태 정보 확인하기
+console.log(store.getState());
+const store = legacy_createStore(reducer);
 root.render(
   <React.StrictMode>
-    <TomatoTalk />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
